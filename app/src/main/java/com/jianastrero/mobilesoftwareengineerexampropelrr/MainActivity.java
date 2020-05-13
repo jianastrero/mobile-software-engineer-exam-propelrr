18bfe8ac56ba1a31e07f97661ac9f4b3e588bec5
Jian James P. Astrero <jianjamesastrero@gmail.com>
2020-05-13 12:05:06 +0800

    set binding and view model on main activity

--- app/src/main/java/com/jianastrero/mobilesoftwareengineerexampropelrr/MainActivity.java
+++ app/src/main/java/com/jianastrero/mobilesoftwareengineerexampropelrr/MainActivity.java
@@ -2,12 +2,22 @@ package com.jianastrero.mobilesoftwareengineerexampropelrr;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+import com.jianastrero.mobilesoftwareengineerexampropelrr.databinding.ActivityMainBinding;
+import com.jianastrero.mobilesoftwareengineerexampropelrr.viewmodel.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+
     }
 }
